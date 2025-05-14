bd6282923672729b41b2316a16141f57ad685116
condino-ciaramarie <ciaramariecondino0901@gmail.com>
2025-05-14 17:02:49 +0800

    Update condino module in main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # === IMPORTS ===
 from taskhub.arguelles import generate_fake_user
 from taskhub.caya import open_image
-from taskhub.condino import make_me_laugh
+from taskhub.condino import generate_joke
 from taskhub.cordova import plot_fake_chem_reactions
 from taskhub.gutierrez import react_with_emoji
 
 # === FUNCTION CALLS ===
 generate_fake_user()
 open_image()
-make_me_laugh()
+generate_joke()
 plot_fake_chem_reactions()
 print(react_with_emoji("Hoorayy! :tada: :partying_face:"))
