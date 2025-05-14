430638ef6a458015e8df5e625a552ecb10a33d26
arguelles-norlan <norlancarguelles@iskolarngbayan.pup.edu.ph>
2025-05-14 13:37:49 +0800

    Add arguelles module to taskhub package

---
+++ taskhub/arguelles.py
@@ -0,0 +1,11 @@
+from faker import Faker
+
+fake = Faker()
+
+def generate_fake_user():
+    fake = Faker()
+    
+    print(f"Name: {fake.name()}")
+    print(f"Birthdate: {fake.date_of_birth()}")
+    print(f"Email: {fake.email()}")
+    print(f"Contact Number: {fake.phone_number()}")
