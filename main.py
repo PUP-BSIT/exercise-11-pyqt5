0a14db297f307b56a62ec4df485c1815b821cf51
condino-ciaramarie <ciaramariecondino0901@gmail.com>
2025-05-14 16:02:55 +0800

    Integrate condino module to main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # === IMPORTS ===
 from taskhub.arguelles import generate_fake_user
 from taskhub.caya import open_image
-# TODO (Condino): Import your module
+from taskhub.condino import make_me_laugh
 from taskhub.cordova import plot_fake_chem_reactions
 from taskhub.gutierrez import react_with_emoji
 
 # === FUNCTION CALLS ===
 generate_fake_user()
 open_image()
-# TODO (Condino): Call your main function
+make_me_laugh()
 plot_fake_chem_reactions()
 print(react_with_emoji("Hoorayy! :tada: :partying_face:"))
