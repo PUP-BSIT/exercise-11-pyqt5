a478eb48d779a7538f459e7d688f579b3e90fbd1
caya-karlchristian <karlcaya@gmail.com>
2025-05-14 13:39:10 +0800

    Integrate caya module into main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # === IMPORTS ===
 # TODO (Arguelles): Import your module
-# TODO (Caya): Import your module
+from taskhub.caya import open_image
 # TODO (Condino): Import your module
 from taskhub.cordova import plot_fake_chem_reactions
 # TODO (Gutierrez): Import your module
 
 # === FUNCTION CALLS ===
 # TODO (Arguelles): Call your main function
-# TODO (Caya): Call your main function
+open_image()
 # TODO (Condino): Call your main function
 plot_fake_chem_reactions()
 # TODO (Gutierrez): Call your main function
