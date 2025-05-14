babc2a5f09e8e3b648b0c9b8fc7e77fa64af9e1b
gutierrez-kingandrei <kingandreibgutierrez@iskolarngbayan.pup.edu.ph>
2025-05-14 14:44:50 +0800

    Integrate gutierrez module to main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from taskhub.arguelles import generate_fake_user
 from taskhub.caya import open_image
 # TODO (Condino): Import your module
 from taskhub.cordova import plot_fake_chem_reactions
-# TODO (Gutierrez): Import your module
+from taskhub.gutierrez import react_with_emoji
 
 # === FUNCTION CALLS ===
 generate_fake_user()
 open_image()
 # TODO (Condino): Call your main function
 plot_fake_chem_reactions()
-# TODO (Gutierrez): Call your main function
+print(react_with_emoji("Hoorayy! :tada: :partying_face:"))
