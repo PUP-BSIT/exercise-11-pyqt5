7d368cf782b46598b618b8787beab2d73346e28d
gutierrez-kingandrei <kingandreibgutierrez@iskolarngbayan.pup.edu.ph>
2025-05-14 12:48:26 +0800

    Add TODOs in main.py for member imports and function calls

---
+++ main.py
@@ -0,0 +1,13 @@
+# === IMPORTS ===
+# TODO (Arguelles): Import your module
+# TODO (Caya): Import your module
+# TODO (Condino): Import your module
+# TODO (Cordova): Import your module
+# TODO (Gutierrez): Import your module
+
+# === FUNCTION CALLS ===
+# TODO (Arguelles): Call your main function
+# TODO (Caya): Call your main function
+# TODO (Condino): Call your main function
+# TODO (Cordova): Call your main function
+# TODO (Gutierrez): Call your main function
