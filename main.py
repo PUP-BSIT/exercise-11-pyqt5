140796f0c08c1d360053325cc074ed9310153e00
cordova-aronstephen <aronstephenscordova@gmail.com>
2025-05-14 13:29:35 +0800

    Integrate cordova module into main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 # TODO (Arguelles): Import your module
 # TODO (Caya): Import your module
 # TODO (Condino): Import your module
-# TODO (Cordova): Import your module
+from taskhub.cordova import plot_fake_chem_reactions
 # TODO (Gutierrez): Import your module
 
 # === FUNCTION CALLS ===
 # TODO (Arguelles): Call your main function
 # TODO (Caya): Call your main function
 # TODO (Condino): Call your main function
-# TODO (Cordova): Call your main function
+plot_fake_chem_reactions()
 # TODO (Gutierrez): Call your main function
