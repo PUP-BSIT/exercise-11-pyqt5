6f40b85815466d7cd8933e96ceafa13742ada0a3
arguelles-norlan <norlancarguelles@iskolarngbayan.pup.edu.ph>
2025-05-14 13:50:54 +0800

    Integrate arguelles module to main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # === IMPORTS ===
-# TODO (Arguelles): Import your module
+from taskhub.arguelles import generate_fake_user
 from taskhub.caya import open_image
 # TODO (Condino): Import your module
 from taskhub.cordova import plot_fake_chem_reactions
 # TODO (Gutierrez): Import your module
 
 # === FUNCTION CALLS ===
-# TODO (Arguelles): Call your main function
+generate_fake_user()
 open_image()
 # TODO (Condino): Call your main function
 plot_fake_chem_reactions()
